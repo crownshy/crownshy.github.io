effd512e00d64e7ac68a97084761c03790e424d6
Stuart Lynn <stuart.lynn@gmail.com>
2024-07-30 02:01:20 +0100

    Inital deploy

--- src/config/footer.js
+++ src/config/footer.js
@@ -22,23 +22,23 @@ export const footerSocials = [
 ]
 
 export const footerLists = [
-	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
+	// {
+	// 	title: 'Landing Pages',
+	// 	items: [
+	// 		{
+	// 			title: 'Landing Page 1',
+	// 			slug: '/landing-pages/landing-1',
+	// 		},
+	// 		{
+	// 			title: 'Landing Page 2',
+	// 			slug: '/landing-pages/landing-2',
+	// 		},
+ //      // {
+	// 		// 	title: 'Landing Page 3',
+	// 		// 	slug: '/landing-pages/landing-3',
+	// 		// },
+	// 	],
+	// },
 	{
 		title: 'Company',
 		items: [
@@ -56,25 +56,25 @@ export const footerLists = [
 			},
 		],
 	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
-	},
+	// {
+	// 	title: 'Theme',
+	// 	items: [
+	// 		{
+	// 			title: 'Get Started',
+	// 			slug: '/theme/get-started',
+	// 		},
+	// 		{
+	// 			title: 'Style Guide',
+	// 			slug: '/theme/style-guide',
+	// 		},
+ //      {
+ //        title: 'Theme Setup',
+ //        slug: '/theme/theme-setup',
+ //      },
+ //      {
+ //        title: 'Customizing Odyssey',
+ //        slug: '/theme/customizing-odyssey',
+ //      }
+	// 	],
+	// },
 ];
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Crown Shy: From Garden to Government `,
+	description: `Democratic and self determination tools for communities of all shapes and sizes`,
+	url: `https://crownshy.github.io`, // No trailing slash!
+	name: `Crown Shy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
