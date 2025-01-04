3db5e759f6215e5eafff9d47fd01c7c0271c58b7
Stuart Lynn <stuart.lynn@gmail.com>
2025-01-04 14:43:15 +0000

    Updating urls

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Crown Shy: From Garden to Government `,
 	description: `Democratic and self determination tools for communities of all shapes and sizes`,
-	url: `https://crownshy.github.io`, // No trailing slash!
+	url: `https://crown-shy.com`, // No trailing slash!
 	name: `Crown Shy`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
